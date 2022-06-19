dd81765872b1b59906b21112c3d231249250b893
Suraj Sharma <sharmasuraj41@gmail.com>
2022-06-19 22:54:56 +0530

    changes

--- client/src/services/apiCalls.js
+++ client/src/services/apiCalls.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 
-const BASE_URL = "http://localhost:5500/api/";
-//const BASE_URL = "https://pin-on-map.herokuapp.com/api/";
+//const BASE_URL = "http://localhost:5500/api/";
+const BASE_URL = "https://pin-on-map.herokuapp.com/api/";
 
 const authRequest = axios.create({
   baseURL: BASE_URL,

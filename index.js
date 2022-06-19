4526363d8a4db8ae9087347c44b2c663e03e53c6
Suraj Sharma <sharmasuraj41@gmail.com>
2022-06-19 22:17:16 +0530

    commit

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ app.use(cors());
 app.use("/api/auth", authRoute);
 app.use("/api/pin", pinRoute);
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("client/build"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5500;
